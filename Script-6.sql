802104120382d693ea05879a7d2871e0ceb0392e
zxcdenjir <119624304+zxcdenjir@users.noreply.github.com>
2024-10-17 15:28:05 +0300

    Delete Script-6.sql

--- Script-6.sql
+++
@@ -1,5 +0,0 @@
-select "CharityName", sum("SponsorshipTarget"), count("SponsorName")
-from "Registration" r 
-join "Sponsorship" s on s."RegistrationId" = r."RegistrationId"
-join "Charity" c on c."CharityId" = r."CharityId"
-group by "CharityName";
