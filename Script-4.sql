2fffa3ec4becd08919cd16be53bd71e4da9ebf7c
zxcdenjir <119624304+zxcdenjir@users.noreply.github.com>
2024-10-17 15:12:27 +0300

    Update Script-4.sql

--- Script-4.sql
+++ Script-4.sql
@@ -3,6 +3,7 @@ select ("LastName" || ' ' || "FirstName") as "RunnerName",
 		date_part('year', AGE(current_date, "DateOfBirth")) as "Age",
 		"Gender",
 		row_number() over (partition by e."EventId" order by "RaceTime") as "Position",
+		row_number() over (partition by e."EventId", g."Gender" order by "RaceTime") as "PositionGender",
 		((re."RaceTime" / 60 / 60)::text || 'ч ' || ((re."RaceTime" / 60 % 60)::text || 'мин ' ||  (re."RaceTime" % 60)::text || 'сек')) AS "RaceTime",
 		et."EventTypeName",
 		m."MarathonName",
@@ -18,7 +19,7 @@ join "RegistrationEvent" re on re."RegistrationId" = r2."RegistrationId"
 join "Gender" g on g."GenderId" = r."GenderId"
 join "EventType" et on et."EventTypeId" = e."EventTypeId" 
 where "RaceTime" is not null and "RaceTime" != 0
-order by e."EventId", "Gender", "Position";
+order by e."EventId", "Position";
 
 select (round(avg("RaceTime") / 60 / 60, 2))::text || 'ч ' || (round(avg("RaceTime") / 60 % 60, 2))::text || 'мин ' ||  (round(avg("RaceTime") % 60, 2))::text || 'сек' AS "AverageRaceTime",
 		count("RaceTime") as "AmountOfRunners"
