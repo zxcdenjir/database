d270a237645324fa7e1f3fbaf832ade25b7ac118
zxcdenjir <119624304+zxcdenjir@users.noreply.github.com>
2024-10-17 15:28:11 +0300

    Delete Script-7.sql

--- Script-7.sql
+++
@@ -1,4 +0,0 @@
-select "Password", "DateOfBirth"::date, date_part('year', AGE(current_date, "DateOfBirth")) as "Age"
-from "User" u
-join "Runner" r on r."Email" = u."Email"
-where "Password" similar to '%[a-z]%' and "Password" similar to '%[0-9]%' and "Password" similar to '%[!@#$%^]%' and length("Password") >= 6 and date_part('year', AGE(current_date, "DateOfBirth")) >= 10
